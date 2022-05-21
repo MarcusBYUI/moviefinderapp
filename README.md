3af9c7b22b52c985aa35dc5e0fbfb1830bac19b1
MarcusBYUI <ojo18002@byui.edu>
2022-05-21 04:53:03 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# moviefinderapp
+# MovieFinderApp
 
 WDD330 Team Project
 
 Authors:
 Ojo-Osasere Ayodeji Marcus
+Kevin Matthew Pena
 Angel Lugo Tehuitzil
+
+
+Trello URL: https://trello.com/invite/b/8zRvXMeA/55497bd3f6259d3c6cef9e186249452b/movie-finder-app
+
---
+++ src/js/externalServices.js
@@ -0,0 +1,39 @@
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw { name: "servicesError", message: res.json() };
+  }
+}
+
+class ExternalServices {
+  constructor() {
+    this.api = "http://www.omdbapi.com/?i=tt3896198&apikey=466aa9b6";
+  }
+
+  async returnMovies(callback) {
+    const response = await fetch(`${this.api}&${callback}`);
+    const data = await convertToJson(response);
+    console.log(data);
+  }
+
+  moviesSearch(name = "home") {
+    //function to check name string and retrun it as a callback to the
+    switch (name) {
+      case "home":
+        const list = ["batman", "superman", 2022];
+        const date = new Date().getFullYear();
+        const index = Math.floor(Math.random() * list.length); //1-3
+
+        return `s=${list[index]}&y=${date}]`;
+
+        // decide the name and year
+        break;
+      default:
+        // s=${name}
+        break;
+    }
+  }
+}
+
+export default ExternalServices;
