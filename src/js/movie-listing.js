53c9084c3ada52fe060397cb274ab1216361101e
rony182 <rony.182@hotmail.com>
2022-05-31 23:42:18 -0300

    adding imports to movie listing

--- src/js/movie-listing.js
+++ src/js/movie-listing.js
@@ -1,6 +1,6 @@
 // This file is to display a list of movies based on user input
 import ExternalServices from "./externalServices";
-
+import { toMovieListing, renderMovies } from "./movie-listingRender";
 import { getUrlParams, loadHeaderFooter } from "./utils";
 
 const services = new ExternalServices();
